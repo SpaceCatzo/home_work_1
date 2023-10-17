4d654582a79012b33b1f9162380d452a8ff52df2
Elisei Senovalov <elisei@invoid.vision>
2023-10-17 20:17:12 +0400

    feat: done

---
+++ pythonProject1/main.py
@@ -0,0 +1,19 @@
+# First task
+
+year = 2023
+
+if year % 2 == 0:
+    print("Високосный год")
+else:
+    print("Обычный год")
+
+# Second task
+
+ticket_num = '123411'
+
+if int(ticket_num[0]) + int(ticket_num[1]) + int(ticket_num[2]) == int(ticket_num[3]) + int(ticket_num[4]) + int(
+        ticket_num[5]):
+
+    print("Счастливый билет")
+else:
+    print("Несчастливый билет")
